ec2185f56c14dfaa9caf34d2ee819d40e8d56cf6
Jeff <jeff@Jeffs-MacBook-Pro.local>
2022-10-27 18:57:49 +0400

    add other cidr

--- README.md
+++ README.md
@@ -12,24 +12,20 @@ ip段信息取自 [all_cn](https://ispip.clang.cn/all_cn.txt)
 
 ```
 /file remove [find name="static_router.rsc"]
-/tool fetch mode=http url="https://raw.githubusercontent.com/jeffok/chn_ros/main/static_router.rsc" \
-dst-path=static_router.rsc
-:if ([:len [/file find name=static_router.rsc]] > 0) do={
-/ip route remove  [/ip route find comment=CN]
+/tool fetch mode=http url="https://raw.githubusercontent.com/jeffok/chn_ros/main/static_router.rsc"\
+dst-path=static_router.rsc 
+:log info ([/file get static_router.rsc contents]) 
 /im file=static_router.rsc
-}
 
 ```
 
 **cn_address-list.rsc** 是往Firewall - address lists 里生ip段列表。
 ```
-/file remove [find name="cn_address-list.rsc"]
-/tool fetch url="https://raw.githubusercontent.com/jeffok/chn_ros/main/cn_address-list.rsc" \
-dst-path=cn_address-list.rsc
-:if ([:len [/file find name=cn_address-list.rsc]] > 0) do={
-/ip firewall address-list remove [find comment="chn_cidr"]
-/import cn_address-list.rsc
-}
+/file remove [find name="static_address_list.rsc"]
+/tool fetch mode=http url="https://raw.githubusercontent.com/jeffok/chn_ros/main/static_address_list.rsc"\
+dst-path=static_address_list.rsc 
+:log info ([/file get static_address_list contents]) 
+/im file=static_address_list.rsc
 ```
 
 感谢地址提供[Clang](https://ispip.clang.cn/)
--- ros-cidr.sh
+++ ros-cidr.sh
@@ -10,19 +10,29 @@ wget --no-check-certificate -c -O all_cn.txt https://ispip.clang.cn/all_cn.txt
 # /ip route add dst-address=223.29.252.0/22 gateway=121.14.41.209 comment=CN
 # /ip route remove  [/ip route find comment=CN]
 
-ppp_name="l2tp-cn"
+net_name="l2tp-cn"
 # static_router (default in chn)
 {
-echo "/ip route"
+echo "/ip route remove  [/ip route find comment=CN]
+/ip route"
 for cidr in $(cat all_cn.txt) ; do
-  echo "add dst-address=$cidr gateway=$ppp_name comment=CN"
+  echo "add dst-address=$cidr gateway=$net_name comment=CN"
 done
 } > ../static_router.rsc
 
-hk_gateway="172.16.252.253"
+# static_address_list (default in chn)
+echo "/ip firewall address-list remove [find comment="chn_cidr"]
+/ip firewall address-list"
+for address in $(cat static_address_list.rsc) ; do
+  echo "add list=CN address=$address comment=chn_cidr"
+done
+} > ../static_address_list.rsc
+
 # hk_static_router
+hk_gateway="172.16.252.253"
 {
-echo "/ip route"
+echo "/ip route remove  [/ip route find comment=CN]
+/ip route"
 
 for cidr in $(cat all_cn.txt) ; do
   echo "add dst-address=$cidr gateway=$hk_gateway comment=CN"
@@ -30,37 +40,37 @@ done
 
 } > ../hk_static_router.rsc
 
-dbx_gateway="l2tp-cn"
 # dbx_static_router
+dbx_gateway="l2tp-cn"
 {
 echo "/ip route remove  [/ip route find comment=DC]
 /ip route remove  [/ip route find comment=Whatsapp]
 /ip route remove  [/ip route find comment=Wechat]
+/ip route remove  [/ip route find comment=CN]
 /ip route
-add dst-address=66.22.212.0/22 gateway=$dbx_gateway comment=DC
-add dst-address=66.22.218.0/23 gateway=$dbx_gateway comment=DC
-add dst-address=185.151.204.0/24 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.128.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.129.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.130.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.133.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.134.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.135.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.136.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.137.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=162.159.138.232/29 gateway=$dbx_gateway comment=DC
-add dst-address=31.13.0.0/16 gateway=$dbx_gateway comment=Whatsapp
-add dst-address=157.240.0.0/16 gateway=$dbx_gateway comment=Whatsapp
-add dst-address=14.22.9.0/24 gateway=$dbx_gateway comment=Wechat
-add dst-address=119.147.4.0/24 gateway=$dbx_gateway comment=Wechat
-add dst-address=129.226.0.0/16 gateway=$dbx_gateway comment=Wechat
-add dst-address=163.60.15.0/24 gateway=$dbx_gateway comment=Wechat
-add dst-address=183.3.224.0/20 gateway=$dbx_gateway comment=Wechat
-add dst-address=203.105.235.0/24 gateway=$dbx_gateway comment=Wechat
+add dst-address=66.22.212.0/22 gateway=$net_name comment=DC
+add dst-address=66.22.218.0/23 gateway=$net_name comment=DC
+add dst-address=185.151.204.0/24 gateway=$net_name comment=DC
+add dst-address=162.159.128.232/29 gateway=$net_name comment=DC
+add dst-address=162.159.129.232/29 gateway=$net_name comment=DC
+add dst-address=162.159.130.232/29 gateway=$net_name comment=DC
+add dst-address=162.159.133.232/29 gateway=$net_name comment=DC
+add dst-address=162.159.134.232/29 gateway=$net_name comment=DC
+add dst-address=162.159.135.232/29 gateway=$net_name comment=DC
+add dst-address=162.159.136.232/29 gateway=$net_name comment=DC
+add dst-address=162.159.137.232/29 gateway=$net_name comment=DC
+add dst-address=162.159.138.232/29 gateway=$net_name comment=DC
+add dst-address=31.13.0.0/16 gateway=$net_name comment=Whatsapp
+add dst-address=157.240.0.0/16 gateway=$net_name comment=Whatsapp
+add dst-address=14.22.9.0/24 gateway=$net_name comment=Wechat
+add dst-address=119.147.4.0/24 gateway=$net_name comment=Wechat
+add dst-address=129.226.0.0/16 gateway=$net_name comment=Wechat
+add dst-address=163.60.15.0/24 gateway=$net_name comment=Wechat
+add dst-address=183.3.224.0/20 gateway=$net_name comment=Wechat
+add dst-address=203.105.235.0/24 gateway=$net_name comment=Wechat
 "
-
 for cidr in $(cat all_cn.txt) ; do
-  echo "add dst-address=$cidr gateway=$dbx_gateway comment=CN"
+  echo "add dst-address=$cidr gateway=$net_name comment=CN"
 done
 
 } > ../dbx_static_router.rsc
